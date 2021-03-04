63278151c1c00896edd2d57570407db765a82c96
lang0909 <lang0909@ajou.ac.kr>
2021-03-04 19:47:24 +0900

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: ["ts-loader"]
+            },
+            {
+                test: /\.css?$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|png|svg)?$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+
+    output: {
+        filename: "main.js",
+        path: __dirname + "/dist",
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    devServer: {
+        contentBase: "./dist"
+    }
+}
